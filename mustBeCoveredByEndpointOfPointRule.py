4ba2463012583e8cf72c79b1de785f9b38fd0dd2
Mauro Carlevaro <mauroctecno@gmail.com>
2019-06-13 22:48:48 -0300

    approach to the solution
    folder structure creation and first approach to the solution of the rule

---
+++ mustBeCoveredByEndpointOfPointRule.py
@@ -0,0 +1,33 @@
+# encoding: utf-8
+
+import gvsig
+import sys
+
+from gvsig import uselib
+uselib.use_plugin("org.gvsig.topology.app.mainplugin")
+
+from org.gvsig.expressionevaluator import ExpressionEvaluatorLocator
+from org.gvsig.topology.lib.api import TopologyLocator
+from org.gvsig.topology.lib.spi import AbstractTopologyRule
+
+from deletePointAction import DeletePointAction
+
+class MustBeCoveredByEndpointOfPointRule(AbstractTopologyRule):
+    
+    geomName = None
+    expression = None
+    expressionBuilder = None
+    
+    def __init__(self, plan, factory, tolerance, dataSet1, dataSet2):
+        AbstractTopologyRule.__init__(self, plan, factory, tolerance, dataSet1, dataSet2)
+        self.addAction(DeletePointAction())
+    
+    def check(self, taskStatus, report, feature1):
+        try:
+            
+        except:
+            ex = sys.exc_info()[1]
+            gvsig.logger("Can't execute rule. Class Name: " + ex.__class__.__name__ + ". Exception: " + str(ex), gvsig.LOGGER_ERROR)
+
+def main(*args):
+    pass
